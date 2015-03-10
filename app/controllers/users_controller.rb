345a790d2e0ea4e5a0544df686678bbd54003795
Cheri <avyishi@gmail.com>
2015-03-09 19:37:52 -0700

    Added user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.order(:username)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+      if @user.save
+        redirect_to users_url
+      else
+        render :new
+      end
+    end
+
+  def update
+
+      if @user.update(user_params)
+        redirect_to users_url
+      else
+        render :edit
+      end
+    end
+
+  def show
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+end
