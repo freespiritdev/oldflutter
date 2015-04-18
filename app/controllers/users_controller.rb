70f83315d51e9276ded461bd0e0f7181e1a8176f
Cheri <avyishi@gmail.com>
2015-04-18 14:42:51 -0700

    Added admin user to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+   before_action :admin_user,     only: :destroy
 
   def index
     @users = User.order(:username)
@@ -51,5 +52,9 @@ private
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
+
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
