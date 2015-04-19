3ee47f4dbb1da16d72d296fa64e8f6f8fe6480a3
Cheri <avyishi@gmail.com>
2015-04-19 15:11:46 -0700

    Added admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317061025) do
+ActiveRecord::Schema.define(version: 20150419221039) do
 
   create_table "flits", force: true do |t|
     t.integer  "user_id",    null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150317061025) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
